a9591f214e1ac9bddfc70f8e7871fb89d54b9c22
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-02-25 18:50:39 +0900

    enum으로 domain 연동 테스트

---
+++ 2024-02-25-domain-enum/src/main/java/jpabook/jpashop/api/domain/OrderItem.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.api.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
+
+@Entity
+@Table(name = "order_item")
+@Getter @Setter
+public class OrderItem {
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private  Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice; // 주문 가격
+
+    private int count; // 주문 수량
+
+}
